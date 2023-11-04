771bd5dd053046b14e0666f048d3d9bc046ed84d
Ninter6 <mc525740@outlook.com>
2023-11-04 11:32:42 +0800

    update

--- sample/nss.cc
+++ sample/nss.cc
@@ -1,9 +1,15 @@
+/*
+ * @Author: Ninter6 mc525740@outlook.com
+ * @Date: 2023-10-02 13:41:01
+ * @LastEditors: Ninter6
+ * @LastEditTime: 2023-11-04 11:31:36
+ */
 #include "seargs.h"
 #include "sebase64.h"
 #include "secompress.h"
 #include <fstream>
 
-#define NSS_VERSION "1.1.0"
+#define NSS_VERSION "1.1.2"
 
 int main(int argc, const char** argv) {
     st::ArgParser args{"Ninter's base64 and compresser program"};
@@ -14,7 +20,8 @@ int main(int argc, const char** argv) {
         .AddOption("--compress", "-p", "compress mode")
         .AddOption("--file", "-f", "indicates the input is a file name")
         .AddOption("--base58", "-E", "use base85 algorithm")
-        .AddOption("--原神启动", {}, "原神, 启动!")
+        .AddOption("--原神启动", {}, "    原神, 启动!"/*中文有bug，没办法*/)
+        .AddOption("--ikun", "-K", "你是我的，我是你的谁？")
         .AddValueOption("--output", "-o", "specify output file")
         .AddSCOption("--version", "-v", "show the program version", []{
             std::cout << "nss version: " << NSS_VERSION << std::endl;
@@ -29,9 +36,11 @@ int main(int argc, const char** argv) {
     std::string code;
     if (args.OptionEnabled("--file")) {
         std::ifstream fs{args.ArgumentValue("Input")};
-        std::stringstream ss;
-        ss << fs.rdbuf();
-        code = ss.str();
+        if (!fs.good()) {
+            std::cout << "File doesn't exist: " + args.ArgumentValue("Input");
+            return -1;
+        }
+        code = {std::istreambuf_iterator{fs}, std::istreambuf_iterator<char>{}};
     } else {
         code = args.ArgumentValue("Input");
     }
@@ -44,11 +53,13 @@ int main(int argc, const char** argv) {
     }
 
     std::function encode = args.OptionEnabled("--base58") ?
-        st::base58::encode : args.OptionEnabled("--原神启动") ?
-        st::原神::encode : st::base64::encode;
+        st::base58::encode : args.OptionEnabled("--原神启动") ? 
+        st::原神::encode : args.OptionEnabled("--ikun") ? 
+        st::ikun::encode : st::base64::encode;
     std::function decode = args.OptionEnabled("--base58") ?
-        st::base58::decode : args.OptionEnabled("--原神启动") ?
-        st::原神::decode : st::base64::decode;
+        st::base58::decode : args.OptionEnabled("--原神启动") ? 
+        st::原神::decode : args.OptionEnabled("--ikun") ? 
+        st::ikun::decode : st::base64::decode;
 
     if (args.OptionEnabled("--encode")) {
         if (args.OptionEnabled("--compress")) {
--- sebase64.h
+++ sebase64.h
@@ -66,7 +66,7 @@ inline std::string decode(const std::string& input) {
 namespace base58 {
 
 // Base58字符集
-std::string base58Chars =
+static const std::string base58Chars = 
     "123456789"
     "ABCDEFGHJKLMNPQRSTUVWXYZ"
     "abcdefghijkmnopqrstuvwxyz";
@@ -94,7 +94,7 @@ inline std::string decode(const std::string& input) {
         int cur = mp[input[i]];
         for (int j = 0; j < tmp.size(); j++)
             tmp[j] = (cur += tmp[j] * 58) & 0xFF, cur >>= 8;
-        while (cur) tmp.push_back(cur & 0xFF), cur >>= 8;
+        while (cur)tmp.push_back(cur & 0xFF), cur >>= 8;
     }
     return {tmp.rbegin(), tmp.rend()};
 }
@@ -103,7 +103,7 @@ inline std::string decode(const std::string& input) {
 
 namespace 原神 {
 
-static std::string 原神启动[] = {"原", "神", "启", "动"};
+static const std::string 原神启动[] = {"原", "神", "启", "动"};
 
 inline std::string encode(const std::string& input) {
     std::string encoded;
@@ -123,6 +123,42 @@ inline std::string decode(const std::string& input) {
         const auto& c = input.substr(i, 3);
         for (int j = 0; j < 4; j++)
             if (c == 原神启动[j]) {
+                b = (b << 2) | j;
+                if (++n == 4) {
+                    decoded.push_back(b);
+                    b = n = 0;
+                }
+                break;
+            }
+    }
+
+    return decoded;
+}
+
+}
+
+namespace ikun {
+
+static const std::string kunChars[] = {"鸡", "你", "太", "美"};
+
+inline std::string encode(const std::string& input) {
+    std::string encoded;
+
+    for (auto c : input)
+        for (int i = 3; i >= 0; i--)
+            encoded.insert(encoded.size(), kunChars[(c >> i*2) & 3]);
+
+    return encoded;
+}
+
+inline std::string decode(const std::string& input) {
+    std::string decoded;
+    uint8_t b = 0, n = 0;
+
+    for (int i = 0; i < input.size(); i += 3) {
+        const auto& c = input.substr(i, 3);
+        for (int j = 0; j < 4; j++)
+            if (c == kunChars[j]) {
                 b = (b << 2) | j;
                 if (++n == 4) {
                     decoded.push_back(b);
